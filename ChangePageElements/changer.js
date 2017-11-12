186006d7d2265b912005d2bba8d36ff6c3616484
Yamamura <SlandShow12345@mail.ru>
2017-11-12 18:00:30 +0300

    Another DOM-HTML example.

---
+++ ChangePageElements/changer.js
@@ -0,0 +1,5 @@
+// We can change HTML elements using DOM-HTML:
+var element = document.getElementById('test');
+element.innerHTML = "MY TEXT IS HERE!";
+element.style.color = "red";
+element.style.fontSize = "140px";
